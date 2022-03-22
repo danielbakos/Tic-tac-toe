7f1540c96971a7cfb4dbe9e8eaaa23e6cf5fb474
danielbakos <100305939+danielbakos@users.noreply.github.com>
2022-03-22 14:53:35 +0100

    Add files via upload

---
+++ tictactoe/tictactoe/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tictactoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            double x_position = e.GetPosition(grid).X;
+            double y_position = e.GetPosition(grid).Y;
+        }
+    }
+}
---
+++ tictactoe/tictactoe/Renderer/Display.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace tictactoe.Renderer
+{
+    public class Display : FrameworkElement
+    {
+        Size size;
+        public void Resize(Size size)
+        {
+            this.size = size;
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            int x = 1;
+            int y = 1;
+
+            double width = size.Width/3;
+            double height = size.Height/3;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), new Rect(1 + j * width, 1 + i*height, width, height));
+                }
+            }
+        }
+    }
+}
